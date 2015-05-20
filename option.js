4b092f57834fafdbf3f2e95ed2f7c8c391ab1d34
张京涛 <zhangjingtaosleep@163.com>
2015-05-20 17:26:06 +0800

    增加清除登录数据

--- option.js
+++ option.js
@@ -4,6 +4,13 @@ $(document).ready(function(){
 		initFlag = false
 		$("#email").attr("disabled","disabled");
 		$("#email").val(localStorage.getItem("email"));
+		$("#clearbtn").removeAttr("disabled");
+		$("#clearbtn").bind("click",function(){
+			localStorage.removeItem("email");
+			localStorage.removeItem("token");
+			alert("清除成功...");
+			window.location.reload();
+         });		
 	}
 	if(initFlag){
 		$("#saveBtn").bind("click",function(){
@@ -20,8 +27,10 @@ $(document).ready(function(){
 					localStorage.setItem("token",data.token);
 					$("#email").attr("disabled","disabled");
 					alert("配置成功！");
+					window.location.reload();
 				}else{
 					alert("注册失败/(ㄒoㄒ)/~~")
+					window.location.reload();
 				}
             }
          });
